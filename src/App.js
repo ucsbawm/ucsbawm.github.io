7fa9b750601bc24467ecae6a43ab9735a6b4f4b0
Joseph Sullivan <josephsullivan256@gmail.com>
2021-12-22 02:25:36 -0800

    first draft

--- src/App.js
+++ src/App.js
@@ -1,38 +1,119 @@
 import Navbar from './Navbar.js';
-import math from './math.jpg';
+import math from './media/math.jpg';
+import banner1 from './media/ucsb_banner.jpg';
+import banner2 from './media/ucsb_beach.png';
+import placeholder from './media/staff/placeholder.png';
+import logo from './media/logo.jpg';
 
 function App() {
   return (
     <div className="App">
-        <section className="bg-white">
+        <section className="bg-slate-50">
           <Navbar></Navbar>
-          
-      
-          <div className="container flex flex-col px-6 py-10 mx-auto space-y-6 lg:h-[32rem] lg:py-16 lg:flex-row lg:items-center">
-              <div className="w-full lg:w-1/2">
-                  <div className="lg:max-w-lg">
-                      <h1 className="text-3xl font-bold tracking-wide text-gray-800">
-                          Association for Women in Mathematics
-                      </h1>
-                      
-                      <div className="mt-8 space-y-5">
-                          <p className="flex items-center -mx-2 text-gray-700">
-                              <span className="mx-2">We are a student chapter for the Association for Women in Mathematics. We are a community that supports and provides resources for women interested in math.</span>
-                          </p>
-                      </div>
-                  </div>
-              </div>
-      
-              <div className="flex items-center justify-center w-full h-96 lg:w-1/2">
-                  <img className="object-cover w-full h-full mx-auto rounded-md lg:max-w-2xl" src={math}></img>
-              </div>
-          </div>
+          <img className="w-full" src={banner1} alt="UCSB campus"></img>
         </section>
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
-        <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FUCSBAWM%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="340" height="500"></iframe>
+        <About />
+        <Events />
+        <Staff />
+        <Contact />
+        <img className="w-full" src={banner2} alt="UCSB beach"></img>
     </div>
+  );
+}
+
+function About() {
+    return (
+      <section id="about" className="bg-slate-50">
+        <div className="container max-w-screen-xl p-10 mx-auto lg:grid lg:grid-cols-2 lg:gap-8">
+          <img className="w-full my-auto rounded-md" src={math} alt="Math Lecture"></img>
+          <div className="w-full sm:p-10">
+            <h1 className="text-5xl font-bold leading-tight text-slate-800">
+                About us.
+            </h1>
+            
+            <p className="mt-10 text-2xl text-slate-700">
+              The Association for Women in Math Student Chapter at UCSB is a club dedicated to empowering and unifying a community of mathematicians, both undergraduate and graduate. We support young women and non-binary individuals who feel that they are one of few in this field of study, and we focus on presenting a variety of speakers, events and opportunities to reach all strands of the mathematical community. All genders are welcome at every meeting.
+            </p>
+          </div>
+        </div>
+      </section>
+    );
+}
+
+function Events() {
+  return (
+    <section id="events" className="bg-slate-200">
+      <div className="container p-0 sm:p-10 max-w-screen-xl mx-auto lg:grid lg:grid-cols-2 lg:gap-8">
+        <div className="w-full p-10 sm:p-10">
+          <h1 className="text-5xl font-bold leading-tight text-slate-800">
+              Events.
+          </h1>
+          
+          <p className="mt-10 text-2xl text-slate-700">
+              We meet [weekly?] at [time/day?]. Pizza? Research and industry opportunities? Social events? Cool math stuff? I actually don't know.
+          </p>
+
+          <a href="https://www.facebook.com/UCSBAWM/"><p className="text-2xl text-cyan-600 underline">
+              Check us out on Facebook.
+          </p></a>
+        </div>
+        <div className="w-full pb-10">
+          <div className="overflow-auto">
+            <iframe className="mx-auto" title="Facebook Page" src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FUCSBAWM%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="340" height="500"></iframe>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+function Staff() {
+  return (
+    <section id="staff" className="bg-slate-50 sm:px-10">
+      <div className="container max-w-screen-xl mx-auto p-10">
+        <div className="text-center text-5xl font-bold text-slate-800">Staff.</div>
+        <div className="mx-auto p-10 overflow-auto">
+          <div className="flex flex-row flex-nowrap lg:flex-wrap lg:justify-center gap-8">
+            <Card img={placeholder} name="Gahl Shemy" position="President" />
+            <Card img={placeholder} name="Hannah Li" position="Treasurer" />
+            <Card img={placeholder} name="Belinda Lehmann" position="Social Chair" />
+            <Card img={placeholder} name="Grace Lemersal" position="Officer" />
+            <Card img={placeholder} name="Elise Alvarez-salazar" position="Secretary" />
+            <Card img={placeholder} name="Emma Opper" position="???" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+function Card(props){
+  return (<div className="bg-white rounded-lg shadow-lg overflow-hidden min-w-fit">
+    <img className="object-cover w-full h-56" src={props.img} alt="avatar"></img>
+    
+    <div className="px-4 w-60 py-5 text-center">
+        <h1 className="block text-3xl font-bold text-slate-800">{props.name}</h1>
+        <span className="text-xl text-slate-700">{props.position}</span>
+    </div>
+  </div>);
+}
+
+function Contact(){
+  return (
+    <section id="contact" className="bg-slate-200">
+      <div className="container max-w-screen-xl p-10 mx-auto lg:grid lg:grid-cols-2 lg:gap-8">
+        <img className="w-full my-auto rounded-md" src={logo} alt="AWM Logo written in Chalk"></img>
+        <div className="w-full sm:p-10">
+          <h1 className="text-5xl font-bold leading-tight text-slate-800">
+              Contact.
+          </h1>
+          
+          <p className="mt-10 text-2xl text-slate-700">
+            Reach us... On facebook? Is there an email?
+          </p>
+        </div>
+      </div>
+    </section>
   );
 }
 
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,29 +1,23 @@
-import ucsb_banner from './ucsb_banner.jpg';
+import ucsb_banner from './media/ucsb_banner.jpg';
 
 export default function Navbar(props) {
 	return (
-	<nav className="bg-no-repeat bg-top bg-cover lg:bg-[url('./ucsb_banner.jpg')] lg:min-h-[20rem]">
-		<div className="p-6 mx-auto lg:flex lg:justify-between lg:items-center">
-			<div className="flex items-center justify-between">
-				<div>
-					<p className="text-2xl font-bold text-gray-800 lg:text-3xl hover:text-gray-700">UCSB AWM</p>
-				</div>
-			</div>
+		<nav className="bg-white shadow">
+        <div className="container px-6 py-3 mx-auto md:flex">
+            <div className="flex items-center justify-between">
+                <div>
+                    <p className="text-2xl font-bold text-gray-800 w-40">UCSB AWM</p>
+                </div>
+            </div>
 
-			
-			<div className="flex flex-col mt-4 space-y-2 lg:-mx-6 lg:mt-0 lg:flex-row lg:space-y-0">
-				<a className="text-gray-700 lg:px-6 hover:text-blue-500"
-					href="#top">About</a>
-				<a className="text-gray-700 lg:px-6 hover:text-blue-500"
-					href="#">Events</a>
-				<a className="text-gray-700 lg:px-6 hover:text-blue-500"
-					href="#">Staff</a>
-				<a className="text-gray-700 lg:px-6 hover:text-blue-500"
-					href="#">FAQ</a>
-				<a className="text-gray-700 lg:px-6 hover:text-blue-500"
-				href="#">Contact</a>
-			</div>
-		</div>
-		
-	</nav>);
-  }
+            <div className="w-full md:flex md:items-center md:justify-between">
+                <div className="flex flex-col px-2 py-3 -mx-4 md:flex-row md:mx-0 md:py-0">
+                    <a href="#about" className="px-2 py-1 font-medium text-gray-700 rounded md:mx-2">About</a>
+					<a href="#events" className="px-2 py-1 font-medium text-gray-700 rounded md:mx-2">Events</a>
+					<a href="#staff" className="px-2 py-1 font-medium text-gray-700 rounded md:mx-2">Staff</a>
+					<a href="#contact" className="px-2 py-1 font-medium text-gray-700 rounded md:mx-2">Contact</a>
+                </div>
+            </div>
+        </div>
+    </nav>);
+}
